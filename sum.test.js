e0c4f4bf8b79914a4b2f1d0ef6dbdee349e85487
anmol wassan <anmol.wassan@finmo.net>
2026-08-09 05:09:55 +0530

    fixed pipeline

--- sum.test.js
+++ sum.test.js
@@ -1,4 +1,4 @@
-const sum = require("./src/index.js");
+const sum = require("./src/main.js");
 
 test("adds 2 + 3 to equal 5", () => {
   expect(sum(2, 3)).toBe(5);
@@ -6,4 +6,8 @@ test("adds 2 + 3 to equal 5", () => {
 
 test("adds 2 + 2 to equal 4", () => {
   expect(sum(2, 2)).toBe(4);
+});
+
+test("http request", () => {
+  expect();
 });
