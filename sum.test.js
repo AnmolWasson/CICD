5e31c9b80512a87c4bc416a2bab043853412f855
anmol wassan <anmol.wassan@finmo.net>
2026-08-08 11:23:38 +0530

    added brach with failed test

--- sum.test.js
+++ sum.test.js
@@ -3,3 +3,7 @@ const sum = require("./index.js");
 test("adds 2 + 3 to equal 5", () => {
   expect(sum(2, 3)).toBe(5);
 });
+
+test("adds 2 + 2 to equal 4", () => {
+  expect(sum(2, 2)).toBe(10);
+});
